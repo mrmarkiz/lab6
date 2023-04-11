23ab4e8be37f9c714603cb6abac02f8995bfbef3
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-11 18:21:20 +0300

    Add "main" code

--- lab6/Program.cs
+++ lab6/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            
+            int choice;
+            do
+            {
+                Console.Write("Enter task to run: ");
+                int.TryParse(Console.ReadLine(), out choice);
+                switch (choice)
+                {
+                    case 1:
+                        Task1.Run();
+                        break;
+                    case 2:
+                        Task2.Run();
+                        break;
+                    case 3:
+                        Task3.Run();
+                        break;
+                }
+            } while (choice != 0);
         }
     }
 }
